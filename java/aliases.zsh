0e2b37272524f58647021a0536d84fb2c4c1939a
Nerses Aznauryan <naznauryan@hotels.com>
2018-07-10 14:45:57 +0100

    adding java versions aliases

--- java/aliases.zsh
+++ java/aliases.zsh
@@ -1,2 +1,5 @@
 #!/bin/sh
-alias mvnci='mvn clean install'
+alias j8="export JAVA_HOME=`/usr/libexec/java_home -v 1.8`"
+alias j9="export JAVA_HOME=`/usr/libexec/java_home -v 9`"
+alias j10="export JAVA_HOME=`/usr/libexec/java_home -v 10`"
+alias mci="mvn clean install"
